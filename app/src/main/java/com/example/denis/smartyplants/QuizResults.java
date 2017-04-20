6475278ca4e411965e476bb4720ce7af06688538
Denis Babeshko <Denis Babeshko>
2017-04-20 15:55:20 -0400

    Merge branch 'master' of C:\Users\denis\AndroidStudioProjects\Project1-master\SmartyPlants with conflicts.

--- app/src/main/java/com/example/denis/smartyplants/QuizResults.java
+++ app/src/main/java/com/example/denis/smartyplants/QuizResults.java
@@ -4,18 +4,33 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.example.denis.smartyplants.model.Score;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
 import static com.example.denis.smartyplants.utils.utils.RESULT;
 
 public class QuizResults extends AppCompatActivity {
     TextView scored;
+    DatabaseReference reference;
+    FirebaseStorage mFirebaseStorage;
+    StorageReference mStorageReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_results);
+        reference = FirebaseDatabase.getInstance().getReference();
         String incomingmsg = getIntent().getStringExtra(RESULT);
         scored = (TextView)findViewById(R.id.score);
 
+
+
+        reference.push().setValue(new Score(incomingmsg));
+        mFirebaseStorage = FirebaseStorage.getInstance();
+        mStorageReference = mFirebaseStorage.getReference();
         if(Integer.parseInt(incomingmsg) >= 60)
         {
             scored.setText("Good job you have passed with a " + incomingmsg);
