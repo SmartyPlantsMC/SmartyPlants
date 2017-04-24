64bab8e3e1ce57813e19ab6ca2b627102f55e41f
Jesse Bruno <brunoj7@montclair.edu>
2017-04-24 14:25:25 -0400

    added new info1

---
+++ app/src/main/java/com/example/denis/smartyplants/model/Score.java
@@ -0,0 +1,14 @@
+package com.example.denis.smartyplants.model;
+
+/**
+ * Created by Jesse on 4/24/17.
+ */
+
+public class Score {
+    private String quizScore;
+
+    public Score(String quizScore){
+        this.quizScore=quizScore;
+    }
+
+}
